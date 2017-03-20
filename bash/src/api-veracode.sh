2b47cb5eab4e9e027e78109b9820db5fbce898db
Dinis Cruz <dinis.cruz@owasp.org>
2017-03-20 16:54:09 +0000

    Set number_attempts back to 100

--- bash/src/api-veracode.sh
+++ bash/src/api-veracode.sh
@@ -314,7 +314,7 @@ function veracode-scan-history-download {
 
 function veracode-scan-history-download-loop
 {
-    local number_attempts=1
+    local number_attempts=100
     local sleep_value=10
     for i in `seq 1 $number_attempts`;
     do
